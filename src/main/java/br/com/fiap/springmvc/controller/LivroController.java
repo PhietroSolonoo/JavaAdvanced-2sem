14eb646688cf5536901e8ab07e755af2960d44d0
PhietroSolonoo <negociosphietro@gmail.com>
2026-08-14 22:48:08 -0300

    feat: Adiciona camada de segurança em cadastro de livro

--- src/main/java/br/com/fiap/springmvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/springmvc/controller/LivroController.java
@@ -2,9 +2,11 @@ package br.com.fiap.springmvc.controller;
 
 import br.com.fiap.springmvc.model.Genero;
 import br.com.fiap.springmvc.model.Livro;
+import br.com.fiap.springmvc.service.LivroService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -12,10 +14,22 @@ import java.util.Arrays;
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
+    private final LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
     @GetMapping("/cadastro")
     public String cadastro(Model model) {
         model.addAttribute("livro", new Livro());
         model.addAttribute("generos", Arrays.asList(Genero.values()));
         return "livroCadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Model model, Livro livro) {
+        livroService.create(livro);
+        return "livrolista";
+
     }
 }
